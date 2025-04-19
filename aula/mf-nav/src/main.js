0cead94868a2a7d5c315e79b0b422cdfb3e19c13
Vitor Lassen <vitor@lassen.com.br>
2025-04-18 23:49:10 -0300

    implement manualmente iforms navigation

--- aula/mf-nav/src/main.js
+++ aula/mf-nav/src/main.js
@@ -3,13 +3,17 @@ const listaDeLinks = document.querySelector('#lista-de-links');
 window.addEventListener('message', (evento) => {
   if( evento.data.type == 'INICIAR'){
   //    console.log(evento.data)
-    evento.data.rotas.forEach(element => {
-      console.log(element)  
+    evento.data.rotas.forEach(rota => {
+      console.log(rota)  
 
       const li = document.createElement('li');
       const ancora = document.createElement('a')
-      ancora.setAttribute('href', element.path);
-      ancora.innerText = element.name;
+      ancora.setAttribute('href', rota.path);
+      ancora.innerText = rota.name;
+      ancora.addEventListener('click', (evt) =>{
+        evt.preventDefault();
+        window.parent.postMessage({type: 'NAVEGAR', destino: rota}, '*')
+      });
       li.appendChild(ancora);
       listaDeLinks.appendChild(li);
       
--- aula/orquestrador/src/main.js
+++ aula/orquestrador/src/main.js
@@ -1,27 +1,58 @@
-import './style.css'
+import "./style.css";
 
 const rotas = [
   {
-    path: '/', 
-    source: '',
-    name: 'Home',
+    path: "/",
+    source: "http://localhost:8002",
+    name: "Home",
   },
   {
-    path: '/produtos', 
-    source: '',
-    name: 'Produtos',
+    path: "/produtos",
+    source: "http://localhost:8003",
+    name: "Produtos",
   },
   {
-    path: '/clientes', 
-    source: '',
-    name: 'Clientes',
+    path: "/clientes",
+    source: "http://localhost:8001",
+    name: "Clientes",
   },
-]
+];
 
-const navIframe = document.querySelector('#mf-nav')
+const navIframe = document.querySelector("#mf-nav");
+const containerIframe = document.querySelector("#container");
 
-navIframe.onload = () =>{
-  navIframe.contentWindow.postMessage({type: 'INICIAR', rotas: rotas}, 'http://localhost:8000')
+const pathAtual = window.location.pathname;
+const mfInicial = rotas.find((r) => r.path == pathAtual);
+if (mfInicial) {
+  containerIframe.setAttribute("src", mfInicial.source);
 }
 
-console.log(navIframe)
+navIframe.onload = () => {
+  navIframe.contentWindow.postMessage(
+    { type: "INICIAR", rotas: rotas },
+    "http://localhost:8000"
+  );
+};
+
+window.addEventListener("message", (event) => {
+  if (event.data.type == "NAVEGAR") {
+    console.log("Hora de navegar para: ", event.data.destino);
+    const mfAlvoNavegacao = rotas.find(
+      (r) => r.path == event.data.destino.path
+    );
+    if (mfAlvoNavegacao) {
+      console.log("sinal verde para nacegar ");
+      containerIframe.setAttribute("src", mfAlvoNavegacao.source);
+      window.history.pushState({}, "", event.data.destino.path);
+    }
+  }
+});
+
+window.addEventListener("popstate", () => {
+  const path = window.location.pathname;
+  const mfAlvoPop = rotas.find((r) => r.path == path);
+  if (mfAlvoPop) {
+    containerIframe.setAttribute("src", mfAlvoPop.source);
+    console.log('pop sendo feito')
+  }
+});
