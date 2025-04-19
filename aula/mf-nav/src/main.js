0da8a0ea85b372a1f25ddb6292b43c2fe09e787e
Vitor Lassen <vitor@lassen.com.br>
2025-04-18 23:21:06 -0300

    passando parametros para mf-nav

---
+++ aula/mf-nav/src/main.js
@@ -0,0 +1,17 @@
+const listaDeLinks = document.querySelector('#lista-de-links');
+
+window.addEventListener('message', (evento) => {
+  if( evento.data.type == 'INICIAR'){
+  //    console.log(evento.data)
+    evento.data.rotas.forEach(element => {
+      console.log(element)  
+
+      const li = document.createElement('li');
+      const ancora = document.createElement('ancora')
+      ancora.innerText = element.name;
+      li.appendChild(ancora);
+      listaDeLinks.appendChild(li);
+      
+    });
+  }
+})
--- aula/orquestrador/src/main.js
+++ aula/orquestrador/src/main.js
@@ -1,3 +1,27 @@
+import './style.css'
+
+const rotas = [
+  {
+    path: '/', 
+    source: '',
+    name: 'Home',
+  },
+  {
+    path: '/', 
+    source: '',
+    name: 'Produtos',
+  },
+  {
+    path: '/', 
+    source: '',
+    name: 'Clientes',
+  },
+]
+
 const navIframe = document.querySelector('#mf-nav')
+
+navIframe.onload = () =>{
+  navIframe.contentWindow.postMessage({type: 'INICIAR', rotas: rotas}, 'http://localhost:8000')
+}
 
 console.log(navIframe)
