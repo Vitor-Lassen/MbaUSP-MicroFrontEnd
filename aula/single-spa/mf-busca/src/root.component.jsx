b0ce37104b1764a12c0ef55917c46b0d43f4e029
Vitor Lassen <vitor@lassen.com.br>
2025-05-05 23:32:33 -0300

    implement single spa

---
+++ aula/single-spa/mf-busca/src/root.component.jsx
@@ -0,0 +1,8 @@
+export default function Root(props) {
+  return (
+    <section>
+      <h1>Ola Mundo!</h1>
+      <p>A partir do mf-busca</p>
+    </section>
+  );
+}
---
+++ aula/single-spa/mf-nav/src/navbar.component.jsx
@@ -0,0 +1,12 @@
+export default function Navbar() {
+  return (
+    <ul>
+      <li>
+        <a href="/">Home</a>
+      </li>
+      <li>
+        <a href="/pedidos">Pedidos</a>
+      </li>
+    </ul>
+  );
+}
